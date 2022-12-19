2fba735341ab549c92cd57a9af4f2a12521a0dd7
MrKnl45 <120123384+MrKnl45@users.noreply.github.com>
2022-12-19 12:00:22 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/MrKnl45/Weebzone"
+    UPSTREAM_REPO = "https://github.com/MrKnl5/Magneto-WZ"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
